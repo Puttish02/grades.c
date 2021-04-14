2ccee725a44c99ff22f62c9221e579ab64cc59a4
Puttish02 <82376006+Puttish02@users.noreply.github.com>
2021-04-14 12:37:30 +0530

    Grades.c

---
+++ Grades.c
@@ -0,0 +1,28 @@
+#include<studio.h>
+main()
+{
+int marks;
+printf("enter marks:");
+scand("%d",&marks);
+if(marks>=85)
+{
+printf("grade A");
+}
+else if(marks>=70 && marks<=84)
+{
+printf("grade B");
+}
+else if(marks>=55 && marks<=69)
+{
+printf("grade C");
+}
+else if(marks>=40 && marks<=54)
+{
+printf("grade D");
+}
+else if (marks>=0 && marks<40)
+{
+printf("grade f");
+}
+return 0;
+}
